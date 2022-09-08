191155ba26d51aeec732392ab0576ef457e34765
Prashabet <prashabet123@gmail.com>
2022-09-08 16:40:09 -0700

    My sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: always 0
  */
-init main (void)
+int main(void)  
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
