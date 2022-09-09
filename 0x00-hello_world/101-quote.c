de8a3bfc4b09216143ddda379fc42f2bcd6f56f6
Prashabet <prashabet123@gmail.com>
2022-09-08 21:48:04 -0700

    My nineth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - print
  *
- * Return: Always 1(successfu)
+ * Return: Always 1 (successful)
  */
 int main(void)
 {
 write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-};
+}
