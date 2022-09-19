3798af88799f957da7b2ae09e848f067a0602830
Prashabet <prashabet123@gmail.com>
2022-09-19 16:49:00 -0700

    My sixth commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,7 @@
  *
  * Return: Nothing
  */
+
 void rev_string(char *s)
 {
 int l, i;
@@ -13,7 +14,9 @@ char ch;
 
 /*find string length without null char*/
 for (l = 0; s[l] != '\0'; ++l);
-/*swap the string by looping to half the string*/{
+
+/*swap the string by looping to half the string*/for (i = 0; i < l / 2; ++i)
+{	
 ch = s[i];
 s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
 s[l - 1 - i] = ch;
