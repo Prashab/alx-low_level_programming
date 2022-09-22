57a50f9b17e1c2f91059c22575b0bdea2ba0b7d1
Prashabet <prashabet123@gmail.com>
2022-09-22 16:30:04 -0700

    My second commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,5 @@
 /**
- * reverse_array - a function that reverses the
- * content of an array of integers
+ * reverse_array - a function that reverses the content of an array of integers
  *
  * @a: pointer to int array
  * @n: is the number of elements to swap
