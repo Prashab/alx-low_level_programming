9fd07787269143367123282daed9420645abfe87
Prashabet <prashabet123@gmail.com>
2022-09-28 14:35:41 -0700

    My first commit

--- 0x07-pointers_arrays_strings/0x08-recursion/main.h
+++ 0x07-pointers_arrays_strings/0x08-recursion/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#include <stdio.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,4 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-#endif /* MAIN_H */
+
+#endif
