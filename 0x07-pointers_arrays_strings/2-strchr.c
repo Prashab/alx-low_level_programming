9c16559e9a76ac9a7c90b4d0689876faa12ebbbe
Prashabet <prashabet123@gmail.com>
2022-09-28 13:45:15 -0700

    My first commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,22 @@
-include "main.h
+nclude <stdio.h>
+#include "main.h"
 
 /**
- * _strchr - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @c: constant
- * Return: s
+ * _strchr - a function that locates a character in a string.
+ * @s: an input string to search in
+ * @c: an input character to locate into string s
+ * Return: returns pointer to c position
  */
-
 char *_strchr(char *s, char c)
 {
-int i;
 
-for (i = 0; s[i] >= '\0'  ; i++)
+while (*s)
 {
-if (s[i] == c)
-{
-return (s + i);
+if (c == *s)
+return (s);
+s++;
 }
+if (c == *s)
+return (s);
+return (NULL);
 }
