974790e5a66d8bb917d27bd5cf26703a6fed3f6b
Prashab <prashabet123@gmail.com>
2022-10-06 15:11:31 -0700

    My second commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,17 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- *  * *malloc_checked - allocates memory using malloc and exit if failed
- *   * @b: int
- *    * Return: pointer to the array initialized or NULL
- *     */
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
 
 void *malloc_checked(unsigned int b)
 {
-		void *new_mem;
+void *new_mem;
 
-			new_mem = malloc(b);
-				if (new_mem == NULL)
-							exit(98);
-					return (new_mem);
+new_mem = malloc(b);
+if (new_mem == NULL)
+exit(98);
+return (new_mem);
 }
